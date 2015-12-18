298da60fbe8e0c2483a6d50f3cd104aaa5586549
Axl Yody <axlyody@gmail.com>
2015-12-18 17:57:05 +0000

    Login homepage and fix active menu

--- application/views/templates/_parts/public_master_header_view.php
+++ application/views/templates/_parts/public_master_header_view.php
@@ -11,6 +11,7 @@
         <link href="//cdnjs.cloudflare.com/ajax/libs/jquery.nanoscroller/0.8.7/css/nanoscroller.min.css" rel="stylesheet">
         <link href="//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/magnific-popup.min.css" rel="stylesheet">
         <link href="<?php echo site_url('style/ph1/css/style.css');?>" rel="stylesheet">
+        <link href="<?php echo site_url('style/ph1/lib/smoke/smoke.min.css');?>" rel="stylesheet">
         <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
         <link href="<?php echo site_url('style/ph1/lib/jquery.niftymodals/css/component.css');?>" rel="stylesheet"> 
         <link href='https://fonts.googleapis.com/css?family=Gloria+Hallelujah' rel='stylesheet' type='text/css'>
@@ -32,6 +33,9 @@
 .navbar-default {
     margin: 0;
 }
+.box-shadow--2dp {
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14), 0 3px 1px -2px rgba(0, 0, 0, .2), 0 1px 5px 0 rgba(0, 0, 0, .12);
+}
 }
 </style>
     </head>
@@ -48,7 +52,7 @@
           <div class="navbar-header logonya">
             <div class="page-title"><span><?php echo $website->title;?></span></div><a href="/" class="navbar-brand"><b style="    font-family: 'Gloria Hallelujah', cursive;
     font-size: 18px;"><?php echo $website->title;?></b></a>
-          </div><a href="#" data-toggle="collapse" data-target="#am-navbar-collapse" class="am-toggle-top-header-menu collapsed"><span class="icon s7-more"></span></a>
+          </div><a href="javascript:;" data-toggle="collapse" data-target="#am-navbar-collapse" class="am-toggle-top-header-menu collapsed"><span class="icon s7-more"></span></a>
          
 
 
@@ -59,7 +63,7 @@
             if($this->ion_auth->logged_in()) {
             ?>
 
-              <li class="dropdown"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="dropdown-toggle"><img src="<?php echo site_url(); ?>public/media/<?php echo $this->ion_auth->user()->row()->pics;?>"> <?php echo $this->ion_auth->user()->row()->first_name;?><span class="angle-down s7-angle-down"></span></a>
+              <li class="dropdown"><a href="javascript:;" data-toggle="dropdown" role="button" aria-expanded="false" class="dropdown-toggle"><img src="<?php echo site_url(); ?>public/media/<?php echo $this->ion_auth->user()->row()->pics;?>"> <?php echo $this->ion_auth->user()->row()->first_name;?><span class="angle-down s7-angle-down"></span></a>
                 <ul role="menu" class="dropdown-menu">
                   <li><a href="<?php echo site_url('admin/user/profile');?>">Profile page</a></li>
                   <li><a href="<?php echo site_url('admin/user/logout');?>">Logout</a></li>
@@ -69,7 +73,7 @@
 
             <?php } else { ?>
               
-               <li class="dropdown"><a href="#" data-modal="login-modal" class="md-trigger">Login</a>
+               <li class="dropdown"><a href="javascript:;" data-modal="login-modal" class="md-trigger">Login</a>
                
               </li>
 
@@ -87,11 +91,9 @@
           <div class="input-group" style="float:right">
             <input type="text"  name="q" class="form-control" placeholder="What do you search for..." style="color:#fff; width: 210px;" id="country" autocomplete="off">
             <ul class="dropdown-menu txtcountry" style="
-                    border: 0px solid #ccc;
+                        border: 0px solid #ccc;
     border: 0px solid rgba(0,0,0,.15);
-             
-    border-radius: 0px;
-    -webkit-box-shadow: 0 0px 0px rgba(0,0,0,.175);
+    border-radius: 3px;
             
             " role="menu" aria-labelledby="dropdownMenu"  id="DropdownCountry"></ul>
             <span class="input-group-btn">
@@ -134,3 +136,6 @@
 </div>
 
 </div>
+
+
+

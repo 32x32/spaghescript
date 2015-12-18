bc271c34af38737accb42b116c9def06b21c73d7
Axl Yody <axlyody@gmail.com>
2015-12-18 17:56:57 +0000

    Login homepage and fix active menu

--- application/views/templates/_parts/public_master_footer_view.php
+++ application/views/templates/_parts/public_master_footer_view.php
@@ -43,48 +43,48 @@
     text-align: left;
     margin-top: 15px;
     color: #fff;
-">Copyright © 2015 <a href="/">M.C Nime</a>. Designed by <a href="https://www.facebook.com/profile.php?id=100007594726461" target="_blank">Spaghetti-san</a>. Please don't copy and respect our work.</a>
+">Copyright © 2015 <a href="/">M.C Nime</a> | <a href="/api">API</a> · <a href="/about">About</a></p>
+
+<p style="text-align:right;
+    margin-top: -30px;">Designed by <a href="https://www.facebook.com/profile.php?id=100007594726461" target="_blank">Spaghetti-san</a></p>
 
 
 
-
-<p style="
-        color: #fff;
-    font-family: 'Gloria Hallelujah', cursive;
-    font-weight: 700;
-    font-size: 18px;
-    background: #00BCD4;
-    float: right;
-    padding: 10px 20px;
-    margin-top: -60px;
-">M.C Nime
-</p>
 
 </div>
 </footer>
 </div>
+</div>
+</div>
 
-<div id="login-modal" class="md-modal colored-header custom-width md-effect-1">
+<div id="login-modal" class="md-modal colored-header custom-width md-effect-3">
                     <div class="md-content">
                       <div class="modal-header">
                         <button type="button" data-dismiss="modal" aria-hidden="true" class="close md-close"><i class="icon s7-close"></i></button>
                         <h3 class="modal-title">Member Area </h3></h3>
                       </div>
+                      
+                   
+              
                       <div class="modal-body form">
+                        
+                            <form action="/u/login" method="post">
                         <div class="form-group">
                           <label>Username</label>
-                          <input type="email" class="form-control">
+                          <input type="email" name="identity" class="form-control">
                         </div>
                         <div class="form-group">
                           <label>Password</label>
-                          <input type="password" class="form-control">
+                          <input type="password" name="password" class="form-control">
                         </div>
-                       
+                       </form>  
                       </div>
                       <div class="modal-footer">
-                    
-                        <button type="button" data-dismiss="modal" class="btn btn-primary md-close">Login</button>
+                       
+                        <button type="button" data-dismiss="modal" id="login" onclick="$('form').submit();" name="submit" class="btn btn-primary">Login</button>
+                      
                       </div>
+                     
                     </div>
                   </div>
                   <div class="md-overlay"></div>
@@ -95,9 +95,82 @@
     <script src="<?php echo site_url('style/ph1/js/main.js'); ?>" type="text/javascript"></script>
     <script src="<?php echo site_url('style/ph1/lib/bootstrap/dist/js/bootstrap.min.js'); ?>" type="text/javascript"></script>
     <script src="<?php echo site_url('style/ph1/lib/theme-switcher/theme-switcher.min.js'); ?>" type="text/javascript"></script>
+    <script src="<?php echo site_url('style/ph1/lib/smoke/smoke.min.js'); ?>" type="text/javascript"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.0.0/jquery.magnific-popup.min.js" type="text/javascript"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/masonry/3.3.2/masonry.pkgd.min.js" type="text/javascript"></script>
     <script src="//static.tumblr.com/8wmayn4/EHUnyp19i/jquery.modaleffects.js" type="text/javascript"></script>
+    
+    <script>
+$(function () {
+    var url = window.location.pathname; //sets the variable "url" to the pathname of the current window
+    var activePage = url.substring(url.lastIndexOf('/') + 1); //sets the variable "activePage" as the substring after the last "/" in the "url" variable
+        $('.am-nav-right li a').each(function () { //looks in each link item within the primary-nav list
+            var linkPage = this.href.substring(this.href.lastIndexOf('/') + 1); //sets the variable "linkPage" as the substring of the url path in each &lt;a&gt;
+
+            if (activePage == linkPage) { //compares the path of the current window to the path of the linked page in the nav item
+                $(this).parent().addClass('active'); //if the above is true, add the "active" class to the parent of the &lt;a&gt; which is the &lt;li&gt; in the nav list
+            }
+        });
+})
+    </script>
+    
+    <?php if(isset($_GET['error']))
+    {
+ 
+        $error = $_GET['error'];
+            switch($error) {
+                case  "101" :
+                echo"
+                <script>
+                window.onload = function(e) {
+                 e.preventDefault();
+                $.smkAlert({
+                    text: 'Username or password invalid',
+                    type: 'danger',
+                    position:'top-center',
+                    icon: 'fa fa-lock',
+                    time: 3
+                  });
+                }; 
+                </script>
+                ";
+                break;
+                case  "201" :
+                echo"
+                <script>
+                window.onload = function(e) {
+                 e.preventDefault();
+                $.smkAlert({
+                    text: 'You do not have sufficient permissions to access this page',
+                    type: 'danger',
+                    position:'top-center',
+                    icon: 'fa fa-lock',
+                    time: 3
+                  });
+                }; 
+                </script>
+                ";
+                break;
+                case  "202" :
+                echo"
+                <script>
+                window.onload = function(e) {
+                 e.preventDefault();
+                $.smkAlert({
+                    text: 'You do not have sufficient permissions to access this page',
+                    type: 'danger',
+                    position:'top-center',
+                    icon: 'fa fa-lock',
+                    permanent: true
+                  });
+                }; 
+                </script>
+                ";
+                break;
+            }    
+    }
+    
+    ?>
     
     <script type="text/javascript">
          $(document).ready(function () {
